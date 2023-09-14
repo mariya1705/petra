23f18fb1a829d2930864615a2a392cef50124fe5
Mariya Fareed <mariyafareed1705@gmail.com>
2023-09-13 20:36:58 -0400

    added cors

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ import uploadRoutes from "./routes/uploadRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
+var cors = require("cors");
+
+var app = express();
+app.use(cors());
 
 connectDB();
 
